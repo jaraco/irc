8235518fa097bd32f2dc268d0056ad6876bfe88c
jrosdahl <none@none>
2003-08-31 14:34:34 +0000

    Created.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+setup(name="python-irclib",
+      version="0.4.0",
+      py_modules=["irclib", "ircbot"],
+      author="Joel Rosdahl",
+      author_email="joel@rosdahl.net",
+      url="http://python-irclib.sourceforge.net")
