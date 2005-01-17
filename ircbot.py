60b5ae6b08509cd456fc258cbdad2ff79d6e28a1
jrosdahl <none@none>
2005-01-17 22:09:07 +0000

    Corrected default decoding of CTCP messages. Patch from Jeff Huang.

--- ircbot.py
+++ ircbot.py
@@ -240,7 +240,8 @@ class SingleServerIRCBot(SimpleIRCClient):
             if len(e.arguments()) > 1:
                 c.ctcp_reply(nm_to_n(e.source()),
                              "PING " + e.arguments()[1])
-        elif e.arguments()[0] == "DCC" and e.arguments()[1] == "CHAT":
+        elif e.arguments()[0] == "DCC" and string.split(e.arguments()[1],
+                                                        " ", 1)[0] == "CHAT":
             self.on_dccchat(c, e)
 
     def on_dccchat(self, c, e):
