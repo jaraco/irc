7da8377ace9f4578d1d4c637b5c90e867afef997
Jason R. Coombs <jaraco@jaraco.com>
2012-01-13 21:24:36 -0500

    Tabs to spaces on pavement.py

--- pavement.py
+++ pavement.py
@@ -5,59 +5,59 @@ from paver.easy import *
 from paver.setuputils import setup
 
 def get_version():
-	"""
-	Grab the version from irclib.py.
-	"""
-	here = os.path.dirname(__file__)
-	irclib = os.path.join(here, 'irclib.py')
-	with open(irclib) as f:
-		content = f.read()
-	VERSION = eval(re.search('VERSION = (.*)', content).group(1))
-	VERSION = '.'.join(map(str, VERSION))
-	return VERSION
+    """
+    Grab the version from irclib.py.
+    """
+    here = os.path.dirname(__file__)
+    irclib = os.path.join(here, 'irclib.py')
+    with open(irclib) as f:
+        content = f.read()
+    VERSION = eval(re.search('VERSION = (.*)', content).group(1))
+    VERSION = '.'.join(map(str, VERSION))
+    return VERSION
 
 def read_long_description():
-	f = open('README')
-	try:
-		data = f.read()
-	finally:
-		f.close()
-	return data
+    f = open('README')
+    try:
+        data = f.read()
+    finally:
+        f.close()
+    return data
 
 setup_params = dict(
-	name="python-irclib",
-	description="IRC (Internet Relay Chat) protocol client library for Python",
-	long_description=read_long_description(),
-	version=get_version(),
-	py_modules=["irclib", "ircbot"],
-	author="Joel Rosdahl",
-	author_email="joel@rosdahl.net",
-	maintainer="Jason R. Coombs",
-	maintainer_email="jaraco@jaraco.com",
-	url="http://python-irclib.sourceforge.net",
-	classifiers = [
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: Developers",
-		"Programming Language :: Python :: 2.3",
-		"Programming Language :: Python :: 2.4",
-		"Programming Language :: Python :: 2.5",
-		"Programming Language :: Python :: 2.6",
-		"Programming Language :: Python :: 2.7",
-	],
+        name="python-irclib",
+        description="IRC (Internet Relay Chat) protocol client library for Python",
+        long_description=read_long_description(),
+        version=get_version(),
+        py_modules=["irclib", "ircbot"],
+        author="Joel Rosdahl",
+        author_email="joel@rosdahl.net",
+        maintainer="Jason R. Coombs",
+        maintainer_email="jaraco@jaraco.com",
+        url="http://python-irclib.sourceforge.net",
+        classifiers = [
+                "Development Status :: 5 - Production/Stable",
+                "Intended Audience :: Developers",
+                "Programming Language :: Python :: 2.3",
+                "Programming Language :: Python :: 2.4",
+                "Programming Language :: Python :: 2.5",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+        ],
 )
 
 @task
 def generate_specfile():
-	with open('python-irclib.spec.in', 'rb') as f:
-		content = f.read()
-	content = content.replace('%%VERSION%%', get_version())
-	with open('python-irclib.spec', 'wb') as f:
-		f.write(content)
+    with open('python-irclib.spec.in', 'rb') as f:
+        content = f.read()
+    content = content.replace('%%VERSION%%', get_version())
+    with open('python-irclib.spec', 'wb') as f:
+        f.write(content)
 
 @task
 @needs('generate_setup', 'generate_specfile', 'minilib', 'distutils.command.sdist')
 def sdist():
-	"Override sdist to make sure the setup.py gets generated"
+    "Override sdist to make sure the setup.py gets generated"
 
 if __name__ == '__main__':
-	setup(**setup_params)
+    setup(**setup_params)
