1ab27cd785543bcbe89ecc4e9d6a4fc96937476a
Jason R. Coombs <jaraco@jaraco.com>
2012-04-16 20:14:10 -0400

    Added test to capture need for DelayedCommand to be sorted by delay

--- tests/test_irclib.py
+++ tests/test_irclib.py
@@ -1,6 +1,20 @@
+import random
+
 import irclib
 
 def test_version():
 	assert 'VERSION' in vars(irclib)
 	assert isinstance(irclib.VERSION, tuple)
 	assert irclib.VERSION, "No VERSION detected."
+
+def test_delayed_command_order():
+	"""
+	delayed commands should be sorted by delay time
+	"""
+	null = lambda: None
+	delays = [random.randint(0, 99) for x in xrange(5)]
+	cmds = sorted([
+		irclib.DelayedCommand(delay, null, tuple())
+		for delay in delays
+	])
+	assert [c.delay for c in cmds] == sorted(delays)
