3b91e7c7df67c347119338f0c1ced3b1c323ebbe
Jason R. Coombs <jaraco@jaraco.com>
2012-10-07 16:57:34 -0400

    Updated changelog and trove classifiers indicating Python 3 support

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup_params = dict(
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
     setup_requires=[
         'hgtools',
