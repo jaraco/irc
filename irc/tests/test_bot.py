c13808543e65a4325ba1e8376bdc4c402efc404a
Jason R. Coombs <jaraco@jaraco.com>
2012-11-07 08:36:31 -0700

    Added test for SingleServerIRCBot construction

--- irc/tests/test_bot.py
+++ irc/tests/test_bot.py
@@ -18,3 +18,16 @@ class TestChannel(object):
         channel = irc.bot.Channel()
         channel.add_user('tester1')
         assert channel.has_user('Tester1')
+
+class TestBot(object):
+    def test_construct_bot(self):
+        bot = irc.bot.SingleServerIRCBot(
+            server_list = [('localhost', '9999')],
+            realname = 'irclibbot',
+            nickname = 'irclibbot',
+        )
+        assert len(bot.server_list) == 1
+        svr = bot.server_list[0]
+        assert svr.host == 'localhost'
+        assert svr.port == '9999'
+        assert svr.password is None
