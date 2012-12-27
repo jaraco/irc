b33f305a241e925e2faf3c1e2df2db23a2af3aca
Jason R. Coombs <jaraco@jaraco.com>
2012-12-27 13:22:06 -0500

    Updated many tests to pass on Python 3

--- irc/client.py
+++ irc/client.py
@@ -494,28 +494,26 @@ class LineBuffer(object):
     >>> b.feed(b'foo\nbar')
     >>> len(b)
     7
-    >>> list(b.lines())
-    ['foo']
+    >>> list(b.lines()) == [b'foo']
+    True
     >>> len(b)
     3
 
     >>> b.feed(b'bar\r\nbaz\n')
-    >>> list(b.lines())
-    ['barbar', 'baz']
+    >>> list(b.lines()) == [b'barbar', b'baz']
+    True
     >>> len(b)
     0
 
     The buffer will not perform any decoding.
     >>> b.feed(b'Ol\xe9\n')
-    >>> list(b.lines())
-    ['Ol\xe9']
+    >>> list(b.lines()) == [b'Ol\xe9']
+    True
 
     The LineBuffer should also act as an iterable.
-    >>> b.feed('iterate\nthis\n')
-    >>> for line in b:
-    ...    print(line)
-    iterate
-    this
+    >>> b.feed(b'iterate\nthis\n')
+    >>> for line, expected in zip(b, [b'iterate', b'this']):
+    ...    assert line == expected
     """
     line_sep_exp = re.compile(b'\r?\n')
 
@@ -543,8 +541,8 @@ class DecodingLineBuffer(LineBuffer):
 
     >>> b = DecodingLineBuffer()
     >>> b.feed(b'bar\r\nbaz\nOl\xc3\xa9\n')
-    >>> list(b.lines())
-    [u'bar', u'baz', u'Ol\xe9']
+    >>> list(b.lines()) == ['bar', 'baz', u'Olé']
+    True
     >>> len(b)
     0
 
@@ -555,8 +553,8 @@ class DecodingLineBuffer(LineBuffer):
     >>> rb = DecodingLineBuffer()
     >>> b.errors = 'replace'
     >>> b.feed(b'Ol\xe9\n')
-    >>> list(b.lines())
-    [u'Ol\ufffd']
+    >>> list(b.lines()) == ['Ol\ufffd']
+    True
     """
     encoding = 'utf-8'
     errors = 'strict'
