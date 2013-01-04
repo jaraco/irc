a5cfd8d52bee4ffcd8cd469e3b2c8b7cf704140e
Jason R. Coombs <jaraco@jaraco.com>
2013-01-04 15:36:54 -0500

    Added a test for something I would like to work - it should be possible to get the next command without deference to the current time.

--- irc/tests/test_schedule.py
+++ irc/tests/test_schedule.py
@@ -37,3 +37,22 @@ def test_periodic_command_fixed_delay():
 		)
 	assert fd.due() == True
 	assert fd.next().due() == False
+
+class TestCommands(object):
+	def test_command_at_noon(self):
+		"""
+		Create a periodic command that's run at noon every day.
+		"""
+		when = datetime.time(12,0)
+		daily = datetime.timedelta(days=1)
+		# convert when to the next datetime matching this time
+		when = datetime.datetime.combine(datetime.date.today(), when)
+		if when < datetime.datetime.now():
+			when += daily
+		cmd = schedule.PeriodicCommandFixedDelay.at_time(
+			when, daily, function=None, arguments=None)
+		assert cmd.due() is False
+		next_cmd = cmd.next()
+		day_from_now = datetime.datetime.now() + daily
+		two_days_from_now = day_from_now + daily
+		assert day_from_now < next_cmd < two_days_from_now
