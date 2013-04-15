01a221676380dea8c9f2cf8aa5ced5fd09fcbcae
Jason R. Coombs <jaraco@jaraco.com>
2013-04-14 21:57:56 -0400

    Added tests for the throttler

--- irc/tests/test_client.py
+++ irc/tests/test_client.py
@@ -1,3 +1,8 @@
+from __future__ import print_function
+
+import itertools
+import time
+
 import pytest
 import mock
 
@@ -35,3 +40,30 @@ class TestHandlers(object):
 		handler2 = irc.client.PrioritizedHandler(1, lambda: 'other')
 		assert not handler1 < handler2
 		assert not handler2 < handler1
+
+class TestThrottler(object):
+	def test_function_throttled(self):
+		"""
+		Ensure the throttler actually throttles calls.
+		"""
+		# set up a function to be called
+		counter = itertools.count()
+		# set up a version of `next` that is only called 30 times per second
+		limited_next = irc.client.Throttler(next, 30)
+		# for one second, call next as fast as possible
+		deadline = time.time() + 1
+		while time.time() < deadline:
+			limited_next(counter)
+		# ensure the counter was advanced about 30 times
+		assert 29 <= next(counter) <= 31
+
+	def test_reconstruct_unwraps(self):
+		"""
+		The throttler should be re-usable - if one wants to throttle a
+		function that's aready throttled, the original function should be
+		used.
+		"""
+		wrapped = irc.client.Throttler(next, 30)
+		wrapped_again = irc.client.Throttler(wrapped, 60)
+		assert wrapped_again.func is next
+		assert wrapped_again.max_rate == 60
