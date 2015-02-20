42df4884ff3fef5388799ca7100fe4d9a0766530
Jason R. Coombs <jaraco@jaraco.com>
2015-02-19 22:49:32 -0500

    Explicitly support Python 3.4 and drop support for Python 3.2

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setup_params = dict(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     install_requires=[
         'six',
