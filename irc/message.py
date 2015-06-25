8a0c17f39fd63a90b24ed79bd5bde4d52622e41d
Jason R. Coombs <jaraco@jaraco.com>
2015-06-25 10:27:50 -0400

    Add tests for tag parsing

--- irc/message.py
+++ irc/message.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 class Tag(object):
     """
@@ -5,6 +6,27 @@ class Tag(object):
     """
     @staticmethod
     def parse(item):
+        r"""
+        >>> Tag.parse('x') == {'key': 'x', 'value': None}
+        True
+
+        >>> Tag.parse('x=yes') == {'key': 'x', 'value': 'yes'}
+        True
+
+        >>> Tag.parse('x=3')['value']
+        '3'
+
+        >>> Tag.parse('x=red fox\\:green eggs')['value']
+        'red fox;green eggs'
+
+        >>> Tag.parse('x=red fox:green eggs')['value']
+        'red fox:green eggs'
+
+        >>> print(Tag.parse('x=a\\nb\\nc')['value'])
+        a
+        b
+        c
+        """
         key, sep, value = item.partition('=')
         value = value.replace('\\:', ';')
         value = value.replace('\\s', ' ')
