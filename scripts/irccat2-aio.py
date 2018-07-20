b4b58c467f374d40fdaa4799f55fe32b68884412
Matt Broach <go.for.dover@gmail.com>
2018-07-20 17:43:12 -0400

    making AioConnection.connect a coroutine and other small fixes

--- scripts/irccat2-aio.py
+++ scripts/irccat2-aio.py
@@ -70,7 +70,6 @@ def main():
             args.server, args.port, args.nickname, password=args.password
         )
     except irc.client.ServerConnectionError as x:
-        print(x)
         sys.exit(1)
 
     try:
